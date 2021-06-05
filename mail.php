b8041ade2cdf3d25e7125f73407ba280720837f1
shakil-ahmmed <85127113+shakil-ahmmed@users.noreply.github.com>
2021-06-05 17:04:04 +0600

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,19 +1,46 @@
 <?php
-if(!empty($_POST["send"])) {
-	$name = $_POST["userName"];
-	$email = $_POST["userEmail"];
-	$subject = $_POST["subject"];
-	$content = $_POST["content"];
-
-	$toEmail = "shakilahmmed9990@gmail.com";
-	$mailHeaders = "From: " . $name . "<". $email .">\r\n";
-	if(mail($toEmail, $subject, $content, $mailHeaders)) {
-	    $message = "Your contact information is received successfully.";
-	    $type = "success";
-	}
-}
-require_once "contact-view.php";
-?>
-    //redirect
-    header("Location:thankyou.html");
-    ?>
+    $filenameee =  $_FILES['file']['name'];
+    $fileName = $_FILES['file']['tmp_name']; 
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $usermessage = $_POST['message'];
+    
+    $message ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $usermessage; 
+    
+    $subject ="Contact from";
+    $fromname ="shakil-ahmmed.github.io/aacom";
+    $fromemail = 'noreply@shakil-ahmmed.github.io/aacom';  //if u dont have an email create one on your cpanel
+    $mailto = 'shakilahmmed9990@gmail.com';  //the email which u want to recv this email
+    $content = file_get_contents($fileName);
+    $content = chunk_split(base64_encode($content));
+    // a random hash will be necessary to send mixed content
+    $separator = md5(time());
+    // carriage return type (RFC)
+    $eol = "\r\n";
+    // main header (multipart mandatory)
+    $headers = "From: ".$fromname." <".$fromemail.">" . $eol;
+    $headers .= "MIME-Version: 1.0" . $eol;
+    $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+    $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+    $headers .= "This is a MIME encoded message." . $eol;
+    // message
+    $body = "--" . $separator . $eol;
+    $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+    $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+    $body .= $message . $eol;
+    // attachment
+    $body .= "--" . $separator . $eol;
+    $body .= "Content-Type: application/octet-stream; name=\"" . $filenameee . "\"" . $eol;
+    $body .= "Content-Transfer-Encoding: base64" . $eol;
+    $body .= "Content-Disposition: attachment" . $eol;
+    $body .= $content . $eol;
+    $body .= "--" . $separator . "--";
+    //SEND Mail
+    if (mail($mailto, $subject, $body, $headers)) {
+        echo "mail send ... OK"; // do what you want after sending the email
+        
+        
+    } else {
+        echo "mail send ... ERROR!";
+        print_r( error_get_last() );
+    }
